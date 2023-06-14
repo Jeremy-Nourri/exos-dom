60a7d9e4481ef012dd5b102a4fc03d143b8efd0a
Jeremy-Nourri <jnourri@outlook.fr>
2023-06-14 11:17:11 +0200

    feat: add files to start the monster slayer project

---
+++ monster-slayer/index.js
@@ -0,0 +1,5 @@
+// Monster Slayer (Optionnel) Vous devrez créer une page web contenant un petit jeu de combat avec un monstre. La page devra contenir au moins :
+
+// * Deux barre de vie dynamique (joueur et monstre) * 4 boutons : attaque, attaque spéciale, soin et abandon * Optionnel : Logs du combats (couleurs différenciées joueur et monstre)
+
+
