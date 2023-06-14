232291e61f12391f254c927eccce54a3b90dd76f
Jeremy-Nourri <jnourri@outlook.fr>
2023-06-14 22:10:01 +0200

    feat: create functions to handler events clic, button attack and special attack

--- monster-slayer/index.js
+++ monster-slayer/index.js
@@ -1,6 +1,5 @@
 "use strict"
 // Monster Slayer (Optionnel) Vous devrez créer une page web contenant un petit jeu de combat avec un monstre. La page devra contenir au moins :
-
 // * Deux barre de vie dynamique (joueur et monstre) * 4 boutons : attaque, attaque spéciale, soin et abandon * Optionnel : Logs du combats (couleurs différenciées joueur et monstre)
 
 
@@ -28,20 +27,26 @@ const container = document.createElement("div");
 container.setAttribute("class", "container");
 document.body.appendChild(container);
 
+/////////// Init
+let healthPlayer = 100;
+let healthMonster = 100;
+
+
 ///////// TOP ELEMENTS
 
 const divTop = createElemWithClass("div", "container__div-top", container);
 
-const divPlayer = createElemWithClass("div", "container__div-fighter", divTop);
-const nameOfPlayer = createElemWithClass("p", "div-top__p", divPlayer );
-const progessBarContainerOfPlayer = createElemWithClass("div", "div-top__progress-bar-container-player", divPlayer);
-const progressbBarOfPlayer = createElemWithClass("div", "div-top__progress-bar-player", progessBarContainerOfPlayer);
+const divPlayer = createElemWithClass("div", "div-top__div-player", divTop);
+const nameOfPlayer = createElemWithClass("p", "div-player__p", divPlayer).textContent = "Player";
+const progessBarContainerOfPlayer = createElemWithClass("div", "div-player__progress-bar-container-player", divPlayer);
+const progressBarOfPlayer = createElemWithClass("div", "div-player__progress-bar-player", progessBarContainerOfPlayer);
+createElemWithClass("p", "progress-bar-player__p", progressBarOfPlayer).textContent = healthPlayer;
 
-const divMonster = createElemWithClass("div", "container__div-fighter", divTop);
-const nameOfMonster = createElemWithClass("p", "div-fighter__p", divMonster);
-const progessBarContainerOfMonster = createElemWithClass("div", "div-top__progress-bar-container-monster", divMonster);
-const progressbBarOfMonster = createElemWithClass("div", "div-top__progress-bar-monster", progessBarContainerOfMonster);
-
+const divMonster = createElemWithClass("div", "div-top__div-monster", divTop);
+const nameOfMonster = createElemWithClass("p", "div-monster__p", divMonster).textContent = "Monster";
+const progessBarContainerOfMonster = createElemWithClass("div", "div-monster__progress-bar-container-monster", divMonster);
+const progressBarOfMonster = createElemWithClass("div", "div-monster__progress-bar-monster", progessBarContainerOfMonster);
+createElemWithClass("p", "progress-bar-monster__p", progressBarOfMonster).textContent = healthMonster;
 
 /////////// ELEMENTS OF THE MIDDLE
 
@@ -54,8 +59,75 @@ const buttonSpecial = createButton("div-middle__button-special", "SPECIAL ATTACK
 const buttonHeal = createButton("div-middle__button-attack", "HEAL", divMiddle);
 const buttonGive = createButton("div-middle__button-give","GIVE UP", divMiddle);
 
-
 /////////// BOTTOM ELEMENTS
 
-const divBottom = createElemWithClass("div", "container__div-middle", container);
+const divBottom = createElemWithClass("div", "container__div-bottom", container);
+const pDivBottom = createElemWithClass("p", "div-bottom__p", divBottom).textContent = "Attacks description";
+
+
+///////////
+
+const pOfMonsterProgressBar = document.querySelector(".progress-bar-monster__p");
+const pOfPlayerProgressBar = document.querySelector(".progress-bar-player__p")
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function substractHealthForAttack() {
+    let randomNumber = getRndInteger(3, 10);
+    healthMonster -= randomNumber;
+    pOfMonsterProgressBar.textContent = healthMonster;
+    let randomNumberMonsterAttack = getRndInteger(5, 10);
+    healthPlayer -= randomNumberMonsterAttack;
+    pOfPlayerProgressBar.textContent = healthPlayer;
+}
+
+function substractHealthForSpecialAttack() {
+    let randomNumber = getRndInteger(10, 20);
+    healthMonster -= randomNumber;
+    pOfMonsterProgressBar.textContent = healthMonster;
+    let randomNumberMonsterAttack = getRndInteger(5, 10);
+    healthPlayer -= randomNumberMonsterAttack;
+    pOfPlayerProgressBar.textContent = healthPlayer;
+}
+
+function addHealthForPlayer() {
+    healthPlayer += 10;
+    let randomNumberMonsterAttack = getRndInteger(5, 10);
+    healthPlayer -= randomNumberMonsterAttack;
+    pOfPlayerProgressBar.textContent = healthPlayer;
+}
+
+function blockHealthValue() {
+    if (healthPlayer <= 0) {
+        buttonAttack.disabled = true;
+        buttonSpecial.disabled = true;
+        return pOfPlayerProgressBar.textContent = 0;
+    }
+    if (healthMonster <= 0) {
+        buttonAttack.disabled = true;
+        buttonSpecial.disabled = true;
+        return pOfMonsterProgressBar.textContent = 0;
+    }
+}
+
+
+///////// events listener 
+
+buttonAttack.addEventListener("click", () => {
+    if (healthPlayer > 0 && healthMonster > 0) {
+        substractHealthForAttack();
+    }
+    blockHealthValue();
+});
+
+buttonSpecial.addEventListener("click", () => {
+    if (healthPlayer > 0 && healthMonster > 0) {
+        substractHealthForSpecialAttack();
+    }
+    blockHealthValue();
+});
+
 
